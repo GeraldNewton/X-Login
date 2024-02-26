de83f813c936d11690be01a55d920ed0d2f8a50b
GeraldNewton <arpitnewton1@gmail.com>
2024-02-27 00:03:30 +0530

    first

---
+++ xlogin/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+function App() {
+  const [display, setDisplay] = useState("form");
+  const [error, setError] = useState(false);
+  const handleForm = (e) => {
+    e.preventDefault();
+    const { Username, Password } = e.target.elements;
+    if (Username.value === "user" && Password.value === "password") {
+      setError(false);
+      setDisplay("login");
+    } else setError(true);
+  };
+  return (
+    <>
+      <h1>Login</h1>
+      {error && <p>Invalid username or password</p>}
+      {display === "form" ? (
+        <form
+          onSubmit={handleForm}
+          style={{ display: "flex", flexDirection: "column", width: "200px" }}
+        >
+          <>
+            <label htmlFor="Username">username: </label>
+            <input type="text" name="Username" required />
+          </>
+          <>
+            <label htmlFor="pass">password: </label>
+            <input type="password" name="Password" required />
+          </>
+          <button type="submit" style={{ width: "30%" }}>
+            Submit
+          </button>
+        </form>
+      ) : (
+        <>
+          <p>Welcome, user!</p>
+          <button onClick={() => setDisplay("form")}>Go Back</button>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
